2542cf10628da3ce5a5fb381c8f61e1427eee1c7
Anshu Pal <64951054+anshuUnity@users.noreply.github.com>
2024-09-28 01:07:00 +0530

    settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = ["podcastapp-bnh5e7d3abhncph2.canadacentral-01.azurewebsites.net", "127.0.0.1"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
